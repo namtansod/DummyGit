0228f3ceb1e36ebdba5461d2f3ac08d36197e4af
MMiOS PPK <namtansod@gmail.com>
2011-09-03 10:19:38 +0700

    initial

---
+++ DummyGit/DummyGitAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DummyGitAppDelegate.h
+//  DummyGit
+//
+//  Created by JOHN MOLLOY on 3/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DummyGitAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
